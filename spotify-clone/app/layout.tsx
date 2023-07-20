4fcbe625c8bb2d1da9f06b60972871f44ef04df4
karthi-prasad77 <karthiprasad44@gmail.com>
2023-07-20 10:15:31 +0530

    Intro & setup commit

---
+++ spotify-clone/app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Spotify Clone',
+  description: 'Listen to your favourite musics...',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
